41f723a460a3323b817dfe58285f9fb04b18902f
lrobins <lrobins@adobe.com>
2015-03-03 20:38:29 -0700

    adds routing for future pages

--- App/js/app.js
+++ App/js/app.js
@@ -1,32 +1,40 @@
-var app = angular.module('pommApp', []);
+var app = angular.module('pommApp', [ngRoute]);
 
-// app.config(function($routeProvider, $httpProvider){
-//   $httpProvider.interceptors.push('httpRequestInterceptor');
+app.config(function($routeProvider){
+  //$httpProvider.interceptors.push('httpRequestInterceptor');
 
-//   $routeProvider
-//   .when('/', {
-//   	templateUrl: 'index.html',
-//   	controller: 'MainCtrl',
-//     // resolve: {
-//     //   allData: function($route, homeService) {
-//     //     return homeService.getAllData();
-//     //   }
-//     // }
-//   })
+  $routeProvider
+  .when('/', {
+  	templateUrl: 'index.html',
+  	controller: 'MainCtrl'
+  })
+  .when('/Rules', {
+  	templateUrl: 'templates/rules.html',
+  	controller: 'TemplateCtrl'
+  })
+  .when('/Legal', {
+  	templateUrl: 'templates/legal.html',
+  	controller: 'TemplateCtrl'
+  })
+  .when('/About', {
+  	templateUrl: 'templates/about.html',
+  	controller: 'TemplateCtrl'
+  })
+  .when('/Pay', {
+  	templateUrl: 'templates/pay.html',
+  	controller: 'TemplateCtrl'
+  })
+  .when('/Login', {
+  	templateUrl: 'templates/login.html',
+  	controller: 'TemplateCtrl'
+  })
+  .when('/states/:state', {
+  	templateUrl: 'templates/states/state-template.html',
+  	controller: 'StateCtrl'
+  })
+  .otherwise ({
+  	redirectTo: '/'
+  })
 
-//   .when('/states/:state', {
-//   	templateUrl: 'templates/states/state-template.html',
-//   	controller: 'StateCtrl',
-//   	// resolve: {
-//   	// 	teamData: function($route, teamService) {
-//    //      return teamService.getTeamData($route.current.params.team)
-//    //    }
-//    //  }
-//   })
-  
-//   .otherwise ({
-//   	redirectTo: '/'
-//   })
 
-
-// });
+});
